0d0971ba4fd50b4b021de293d8a9ca0754213700
EpicSimen <ezk974@gmail.com>
2021-06-06 06:33:28 +0200

    Organisation des fichiers de configuration

---
+++ config/reglages.js
@@ -0,0 +1,7 @@
+let 
+    // nombre de minutes et de secondes que doit durer le timer
+    minutesTimer = 1,
+    secondesTimer = 0, 
+    // phrase affiché à la fin du timer
+    // mettre un <br /> pour aller à la ligne
+    phraseApresTimer = "Le stream <br/> va commencer !";
